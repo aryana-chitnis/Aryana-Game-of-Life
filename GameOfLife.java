7a14649d3598d235148bed7a4a16fea4f91a675e
aryana-chitnis <ac1000@pleasantonusd.net>
2025-03-08 04:31:52 +0000

    completed steps 1 -3

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,27 +1,27 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class GameOfLife implements Board {
-
     // Integers: 0 or 1 for alive or dead
-    private int[][] board;
+    private ArrayList<ArrayList<Integer>> grid;
 
-    public GameOfLife(int x, int y)
-    {
-        // Construct a 2d array of the given x and y size.
-    }
-
-    // Set values on the board
-    public void set(int x, int y, int[][] data) {
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[0].length; j++) {
-                board[i + x][j + y] = data[i][j];
+    public GameOfLife(int rows, int cols) {
+        // Construct a 2D ArrayList of the given dimensions
+        grid = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < cols; j++) {
+                row.add(0);
             }
+            grid.add(row);
         }
     }
 
     // Run the simulation for a number of turns
     public void run(int turns) {
-        // call step the number of times requested
+        for (int i = 0; i < turns; i++) {
+            step();
+        }
     }
 
     // Step the simulation forward one turn.
--- TestSuite.java
+++ TestSuite.java
@@ -12,21 +12,21 @@ public class TestSuite {
         int[][] data = {{ 1 }};
 
         // Set a single cell, then check that it has zero neighbors.
-        board.set(2, 2, data);
-        pass &= expect(board.countNeighbors(2,2), 0, "Single live cell with zero neighbors");
+        board.set(2, 3, data);
+        pass &= expect(board.countNeighbors(2,3), 0, "Single live cell with zero neighbors");
 
         // Set the middle top neighbor.
         board.set(1, 2, data);
-        pass &= expect(board.countNeighbors(2,2), 1, "Single live cell with one neighbor");
+        pass &= expect(board.countNeighbors(2,3), 1, "Single live cell with one neighbor");
 
         // Set the bottom middle neighbor.
         board.set(3, 2, data);
-        pass &= expect(board.countNeighbors(2,2), 2, "Single live cell with two neighbors");
+        pass &= expect(board.countNeighbors(2,3), 2, "Single live cell with two neighbors");
 
         // Test update state
         board.step();
         board.print();
-        pass &= expect(board.get(2,2), 1, "Expect line to rotate");
+        pass &= expect(board.get(2,3), 1, "Expect line to rotate");
 
         if (pass == true)
         {
